17b612e5063a747f47466796c6cf14cde73566bc
qualimock <qualimock@gmail.com>
2024-11-21 02:01:49 +0300

    obtain repositories names with availability check

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,15 @@ fn main() {
 	if args.len() == 1  {
 		panic!("No repository provided");
 	}
+
+	let branch_a_name = &args[1];
+	let branch_b_name = &args[2];
+
+	if !available_repos.contains(&branch_a_name.as_str()) {
+		panic!("No such repository {branch_a_name}.\nAvailable repos: {available_repos:?}");
+	}
+
+	if !available_repos.contains(&branch_b_name.as_str()) {
+		panic!("No such repository {branch_b_name}.\nAvailable repos: {available_repos:?}");
+	}
 }
