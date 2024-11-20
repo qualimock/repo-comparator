b7f518dc803d01331e9e55143b25b8b44ed0df5a
qualimock <qualimock@gmail.com>
2024-11-21 02:00:13 +0300

    add list of available repositories

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
 
 fn main() {
+	let available_repos = vec![
+		"p9",
+		"p10",
+		"p11",
+		"sisyphus"
+	];
+
 	let args: Vec<String> = env::args().collect();
 
 	if args.len() == 1  {
